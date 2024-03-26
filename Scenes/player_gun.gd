a4214819197b71423607ecd681001ff54198cf08
Revenant <joelpeters2017@gmail.com>
2024-03-26 05:05:37 -0400

    dashes

--- Scenes/player_gun.gd
+++ Scenes/player_gun.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var max_speed: int = 1500
 var speed: int = max_speed
+var dash_distance: int = 150
 
 func _process(_delta):
 	
@@ -13,4 +14,6 @@ func _process(_delta):
 	# rotate
 	look_at(get_global_mouse_position())
 	var player_direction = ((get_global_mouse_position() - position).normalized())
-
+	
+	if Input.is_action_just_pressed("shift"):
+		set_global_position(Vector2(player_direction.x * dash_distance + Globals.player_pos.x, player_direction.y * dash_distance + Globals.player_pos.y))
