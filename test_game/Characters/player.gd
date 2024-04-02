d5e01b301d1a7caf6a14c87295e5aed20d2867f8
pharmony01 <rileypeters2016@gmail.com>
2024-04-02 01:11:41 -0400

    Custom crosshair, started functionality for left/right click

--- test_game/Characters/player.gd
+++ test_game/Characters/player.gd
@@ -1,5 +1,7 @@
 extends Character
 
+const LASER_SPEED = 1000.0
+const GRENADE_SPEED = 500.0
 
 func _process(_delta: float) -> void:
 	var mouse_direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
@@ -15,3 +17,8 @@ func get_input() -> void:
 		mov_direction += Vector2.RIGHT
 	if Input.is_action_pressed("move_up"):
 		mov_direction += Vector2.UP
+	if Input.is_action_just_pressed("primary"):
+		print("Shoot")
+	if Input.is_action_just_pressed("secondary"):
+		print("Grenade")
+	
