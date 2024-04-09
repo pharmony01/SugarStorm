614f68463346f3f08df4d8134fa37508dc0333cb
pharmony01 <rileypeters2016@gmail.com>
2024-04-09 03:41:58 -0400

    Title screen with epic graphics

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -8,3 +8,18 @@ func _on_start_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_start_button_mouse_entered():
+	$StartButton.scale = Vector2(1.35, 1.35)
+	
+func _on_start_button_mouse_exited():
+	$StartButton.scale = Vector2(1, 1)
+
+
+func _on_quit_button_mouse_entered():
+	$QuitButton.scale = Vector2(1.35, 1.35)
+
+
+func _on_quit_button_mouse_exited():
+	$QuitButton.scale = Vector2(1, 1)
