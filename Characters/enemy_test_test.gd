a6002ce40cb180ef78a6cfcf11fd7344f1fa5122
pharmony01 <rileypeters2016@gmail.com>
2024-04-09 02:24:36 -0400

    Added title screen with evenly spaced buttons!

--- Characters/enemy_test_test.gd
+++ Characters/enemy_test_test.gd
@@ -6,13 +6,14 @@ func _process(_delta):
 
 
 func _on_player_moved(player_new_position):
+	pass
 	# Calculate direction vector from enemy to player
-	var direction = player.global_position - global_position
+	# var direction = player.global_position - global_position
 	# Normalize the direction vector
-	direction = direction.normalized()
+	# direction = direction.normalized()
 	# Calculate movement vector
-	var movement = direction * speed * delta
+	# var movement = direction * speed * delta
 		# Move the enemy
-	move_and_slide(movement)
+	# move_and_slide(movement)
 	# calculate the direction of the player using 'player_new_position'
 	# and multiply the result with the enemy speed and plug it all in move_and_slide()
---
+++ TitleScreen.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
