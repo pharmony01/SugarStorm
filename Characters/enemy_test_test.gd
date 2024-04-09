ded6cd60604b4a1bc1c2500eafbf5aaf0f3eba37
Revenant <joelpeters2017@gmail.com>
2024-04-09 01:21:30 -0400

    first enemy

---
+++ Characters/enemy_test_test.gd
@@ -0,0 +1,18 @@
+extends Character
+
+func _process(_delta):
+	look_at(get_global_mouse_position())
+	
+
+
+func _on_player_moved(player_new_position):
+	# Calculate direction vector from enemy to player
+	var direction = player.global_position - global_position
+	# Normalize the direction vector
+	direction = direction.normalized()
+	# Calculate movement vector
+	var movement = direction * speed * delta
+		# Move the enemy
+	move_and_slide(movement)
+	# calculate the direction of the player using 'player_new_position'
+	# and multiply the result with the enemy speed and plug it all in move_and_slide()
--- Characters/player.gd
+++ Characters/player.gd
@@ -3,9 +3,14 @@ extends Character
 const LASER_SPEED = 1000.0
 const GRENADE_SPEED = 500.0
 
+signal position_changed(position)
+
+
 func _process(_delta: float) -> void:
 	var mouse_direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
 	look_at(get_global_mouse_position())
+	# emit_signal("position_changed", self.global_position)
+	position_changed.emit(self.global_position)
 	
 func get_input() -> void:
 	mov_direction = Vector2.ZERO
@@ -22,3 +27,7 @@ func get_input() -> void:
 	if Input.is_action_just_pressed("secondary"):
 		print("Grenade")
 	
+
+
+func _on_position_changed(position):
+	pass # Replace with function body.
