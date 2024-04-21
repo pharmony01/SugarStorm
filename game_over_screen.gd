8701008ee1a833a7594fbba6f30d2eff77e5dfbc
pharmony01 <rileypeters2016@gmail.com>
2024-04-21 04:50:01 -0400

    End game screen

--- game_over_screen.gd
+++ game_over_screen.gd
@@ -2,9 +2,23 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var survival_time = Time.get_ticks_msec() / 1000.0
+	var survival_time = Time.get_ticks_msec() / 1000
+	var lived = "You survived for 0d 0h %sm %ss" % [survival_time / 60, survival_time % 60]
+	$TimeLived.set_text(lived)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_continue_button_mouse_entered():
+	$ContinueButton.scale = Vector2(1.35, 1.35)
+
+
+func _on_continue_button_mouse_exited():
+	$ContinueButton.scale = Vector2(1, 1)
+
+
+func _on_continue_button_pressed():
+	get_tree().change_scene_to_file("res://title_screen.tscn")
